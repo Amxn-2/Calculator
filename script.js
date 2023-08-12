c66dd31d3058e482db5b9a949e25e0675733f2f2
Amxn-2 <96385079+Amxn-2@users.noreply.github.com>
2023-08-12 11:09:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,21 +3,50 @@ const buttons = document.querySelectorAll("button");
 
 buttons.forEach((item) => {
   item.onclick = () => {
-    if (item.id == "clear") {
+    if (item.id === "clear") {
       display.innerText = "";
-    } else if (item.id == "backspace") {
+    } else if (item.id === "backspace") {
       let string = display.innerText.toString();
       display.innerText = string.substr(0, string.length - 1);
-    } else if (display.innerText != "" && item.id == "equal") {
-      display.innerText = eval(display.innerText);
-    } else if (display.innerText == "" && item.id == "equal") {
-      display.innerText = "Empty!";
-      setTimeout(() => (display.innerText = ""), 2000);
+    } else if (item.id === "equal") {
+      try {
+        let expression = display.innerText;
+        let balanced = areParenthesesBalanced(expression);
+        
+        if (!balanced) {
+          display.innerText = "Invalid Parentheses!";
+          setTimeout(() => (display.innerText = ""), 2000);
+        } else if (expression !== "") {
+          display.innerText = eval(expression);
+        } else {
+          display.innerText = "Empty!";
+          setTimeout(() => (display.innerText = ""), 2000);
+        }
+      } catch (error) {
+        display.innerText = "Error!";
+        setTimeout(() => (display.innerText = ""), 2000);
+      }
     } else {
       display.innerText += item.id;
     }
   };
 });
+
+// Helper function to check if parentheses are balanced
+function areParenthesesBalanced(expression) {
+  const stack = [];
+  for (const char of expression) {
+    if (char === "(") {
+      stack.push(char);
+    } else if (char === ")") {
+      if (stack.length === 0) {
+        return false;
+      }
+      stack.pop();
+    }
+  }
+  return stack.length === 0;
+}
 
 const themeToggleBtn = document.querySelector(".theme-toggler");
 const calculator = document.querySelector(".calculator");
